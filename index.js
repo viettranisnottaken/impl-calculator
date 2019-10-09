bdf4fe7a4fb805704db2da16ce0a127c10a07625
viettranisnottaken <tranxuanviet1997@gmail.com>
2019-10-09 16:24:13 +0700

    wip

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // e.target.getAttribute("class").includes("keyOperator")
 // 2 types of zero
+// rewrite + - * /
+// separate spring with the operator as the separator: string.split(",")
 
 const keyPlus = document.getElementById("key+");
 const keyMinus = document.getElementById("key-");
@@ -13,40 +15,69 @@ var screen = document.getElementById("screen");
 var key = document.getElementsByClassName("key");
 var result;
 var calculated;
+var calculate;
+var arrayOfOperand;
+var operator;
+var fn;
 
-var checkCalculated = function() {
-  if (calculated === true) {
-    return screen.innerHTML = "0"
-    // return (e.target.getAttribute("class").includes("keyOperator")
-    //   ? (screen.innerHTML = this.value)
-    //   : (screen.innerHTML = "0"))
-  }
+var add = function(a, b) {
+  return screen.innerHTML = a + b;
 };
 
+var substract = function(a, b) {
+  return screen.innerHTML = a - b;
+};
+
+var multiply = function(a, b) {
+  return screen.innerHTML = a * b;
+};
+
+var divide = function(a, b) {
+  return screen.innerHTML = a / b;
+};
+
+var getOperand = function() {
+  operator = screen.innerHTML.match(/[+\-*/]/);
+  return arrayOfOperand = screen.innerHTML.split(operator[0]);
+}
+
+var getOperation = function() {
+  operator = screen.innerHTML.match(/[+\-*/]/);
+  switch (operator[0]) {
+    case "+":
+      fn = add
+      break;
+    case "-":
+      fn = substract
+      break;
+    case "*":
+      fn = multiply
+      break;
+    case "/":
+      fn = divide
+  }
+
+  // debugger;
+
+  // return window["add"](parseInt(arrayOfOperand[0]), parseInt(arrayOfOperand[1]))
+}
+
+var calculate = function() {
+  return fn(parseInt(arrayOfOperand[0]), parseInt(arrayOfOperand[1]))
+}
+
 for (let index = 0; index < key.length; index++) {
-  key[index].addEventListener("click", function(e) {
-    checkCalculated();
-    calculated = false;
-    // debugger;
-    if (screen.innerHTML === "0") {
-      return (e.target.getAttribute("class").includes("keyOperator") 
-        ? (screen.innerHTML = this.value)
-        : (screen.innerHTML = parseInt(this.value)));
-    } else {
-      return (e.target.getAttribute("class").includes("keyOperator")
-        ? (screen.innerHTML = screen.innerHTML + this.value)
-        : (screen.innerHTML = screen.innerHTML + parseInt(this.value)));
-    }
-  })  
-}
+  key[index].addEventListener("click", function() {
+    return screen.innerHTML += this.value;
+  });  
+};
 
-function calculate() {
-  calculated = true
-  return screen.innerHTML = eval(screen.innerHTML);
-}
-
-keyEqual.onclick = calculate;
 keyAC.onclick = function() {
-  calculated = false;
-  return screen.innerHTML = "0"
+  return screen.innerHTML = "";
+};
+
+keyEqual.onclick = function() {
+  getOperand();
+  getOperation();
+  calculate();
 }
